d7f867a527f893fd11a2eb3765a812ba84a77863
Mike Møller Nielsen <mmni@protonmail.com>
2020-05-04 23:26:42 +0200

    files

---
+++ initgit.sh
@@ -0,0 +1,6 @@
+echo "# maven-lombok-annotation-compiler" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:ekim197711/maven-lombok-annotation-compiler.git
+git push -u origin master
---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! " +
+                new Train("T800", 140.5f));
+    }
+}
